a9cf3766c55028a34fd6db519a4a7c8ef05be2a8
ifoster01 <ifoster41901@gmail.com>
2024-04-19 18:12:02 -0400

    fixed some vercel issues

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,5 +1,6 @@
 import { createClient } from "@/utils/supabase/server";
 import { NextResponse } from "next/server";
+import { headers } from "next/headers";
 
 export async function GET(request: Request) {
   // The `/auth/callback` route is required for the server-side auth flow implemented
@@ -7,17 +8,20 @@ export async function GET(request: Request) {
   // https://supabase.com/docs/guides/auth/server-side/nextjs
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
-  // const origin = requestUrl.origin;
+  const origin = requestUrl.origin;
 
   if (code) {
     const supabase = createClient();
     await supabase.auth.exchangeCodeForSession(code);
   }
   
-  const login = requestUrl.searchParams.get('login');
+  const login = requestUrl.searchParams.get("login");
+  // const origin = headers().get("origin");
+
+  console.log(origin)
 
   if (login === "true") {
-    return NextResponse.redirect(`${process.env.CALLBACK_URL}/authed`);
+    return NextResponse.redirect(`${origin}/authed`);
   }
 
   // If the user is signing up, create a new user in the database
@@ -25,7 +29,7 @@ export async function GET(request: Request) {
   const { data: user, error: error2 } = await supabase.auth.getUser();
 
   if (error2) {
-    return NextResponse.redirect(`${process.env.CALLBACK_URL}/login`);
+    return NextResponse.redirect(`${origin}/login`);
   }
 
   const { error: error3 } = await supabase
@@ -41,9 +45,9 @@ export async function GET(request: Request) {
   }]);
 
   if (error3) {
-    return NextResponse.redirect(`${process.env.CALLBACK_URL}/login`);
+    return NextResponse.redirect(`${origin}/login`);
   }
 
   // URL to redirect to after sign up process completes
-  return NextResponse.redirect(`${process.env.CALLBACK_URL}/authed`);
+  return NextResponse.redirect(`${origin}/authed`);
 }
